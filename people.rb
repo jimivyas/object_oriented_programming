d17af5b9cfa51db558e8752a4ea894252149a80a
Jimi Vyas <jimipvyas@gmail.com>
2014-10-23 14:05:42 -0400

    added say_name method to Person, made it attr_accessor

--- people.rb
+++ people.rb
@@ -1,9 +1,14 @@
 
-# Both the instructor and the student have names. We know that instructors and students are both people. Create a parent Person class that contains the attribute name and an initializer to set the name.
+#Both the instructor and the student have names. We know that instructors and students are both people. Create a parent Person class that contains the attribute name and an initializer to set the name.
 
-class Person
+class Person 
+	attr_accessor :name
 	def initialize(name)
 		@name = name
+	end
+
+	def say_name
+		puts "Hi, my name is #{@name}"
 	end
 end
 
@@ -11,16 +16,16 @@ end
 
 
 
-class Student
-	# The student class has a method called learn that outputs "I get it!".
+class Student < Person
+	#The student class has a method called learn that outputs "I get it!".
 	def learn
 		puts "I get it!"
 	end
 
 end
 
-class Instructor
-	# The instructor class has a method called teach that outputs "Everything in Ruby is an Object".
+class Instructor < Person
+	#The instructor class has a method called teach that outputs "Everything in Ruby is an Object".
 	def teach
 		puts "Everything in Ruby is an object"
 	end
