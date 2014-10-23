fd1526ba181032279c7b82d2f7a87566510a6e8d
Jimi Vyas <jimipvyas@gmail.com>
2014-10-23 13:58:46 -0400

    added person class with initialize

--- people.rb
+++ people.rb
@@ -1,8 +1,17 @@
-# Let's start by creating two classes: one called Student and another called Instructor.
+
+# Both the instructor and the student have names. We know that instructors and students are both people. Create a parent Person class that contains the attribute name and an initializer to set the name.
+
+class Person
+	def initialize(name)
+		@name = name
+	end
+end
+
+
+
+
 
 class Student
-
-
 	# The student class has a method called learn that outputs "I get it!".
 	def learn
 		puts "I get it!"
@@ -11,17 +20,16 @@ class Student
 end
 
 class Instructor
+	# The instructor class has a method called teach that outputs "Everything in Ruby is an Object".
 	def teach
 		puts "Everything in Ruby is an object"
 	end
-	
+
 end
 
 
 
-# The instructor class has a method called teach that outputs "Everything in Ruby is an Object".
 
-# Both the instructor and the student have names. We know that instructors and students are both people. Create a parent Person class that contains the attribute name and an initializer to set the name.
 
 # Both the instructor and the student should also be able to do a greeting, like "Hi, my name is #{name}". Where's the best place to put this common method?
 
